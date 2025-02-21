70d3f4157ff4c2545dcab0c9279e9f75499df8a5
Youness SBAI <youness.sbai.work@gmail.com>
2025-02-21 13:24:21 +0100

    Erased all evidence of my past mistakes. Fresh start!

--- include/input.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "./config.h"
-
-// Function used to read an input from the user
-extern bool read_user_input(uint8_t *input);
-
-// Function used to clear the input buffer
-extern void clear_input_buffer();
